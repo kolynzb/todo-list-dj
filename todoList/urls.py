fadace1404bd77dd314690d98f24bff6c4d8705f
kolynzb <collinsbenda360@gmail.com>
2023-08-01 13:00:30 +0300

    feat: add api url endpoints

--- todoList/urls.py
+++ todoList/urls.py
@@ -1,24 +1,34 @@
-"""todolist URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path,include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
+admin.site.site_header = 'Todo Admin'
+admin.site.index_title = 'Admin'
 
+# Schema view for API documentation
+schema_view = get_schema_view(
+    openapi.Info(
+        title="todo API",
+        default_version='v1.0.0',
+        description="TOdo 🚀 Official API Documentation",
+        terms_of_service="https://www.todo.com/terms",
+        contact=openapi.Contact(email="tododev@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+# Define URL patterns for the project
 urlpatterns = [
+    # URL pattern for the ReDoc API documentation UI
+    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # URL pattern for the Django admin site
     path('admin/', admin.site.urls),
-    path('api/',include('api.urls')),
-  
+    # URL pattern for the Swagger API documentation UI
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # URL patterns for the API endpoints (version 1)
+    path('api/v1/',include('api.urls')),
 ]
